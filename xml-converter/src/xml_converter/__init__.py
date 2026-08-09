0ebaf2968ffc7fa0383ac36f65b31929a54a9153
Karlo Cehulic <karlocehlic@gmail.com>
2026-08-09 23:53:40 +0200

    feat: write a new xml

--- xml-converter/src/xml_converter/__init__.py
+++ xml-converter/src/xml_converter/__init__.py
@@ -1,15 +1,22 @@
 import os
 from lxml import etree
 from lxml import objectify
+from lxml import builder
 
 def main() -> None:
     print("Hello from xml-converter!")
-    rel = "../../xml-examples/foo.xml"
+    root_in = root_from_examples("metro_input.xml")
+    tree_out = root_from_examples("metro_output.xml")
+    root_out = tree_out.getroot()
+
+    root_out["Zaglavlje"]["Program"] = "Caffe"
+    objectify.deannotate(tree_out, cleanup_namespaces=True)
+    tree_out.write("new_xml.xml")
+
+def root_from_examples(filename: str):
+    rel = "../../xml-examples/" + filename
     abs = os.path.dirname(__file__)
     abs_target = os.path.join(abs, rel)
     xml_file = open(os.path.abspath(abs_target))
     x = objectify.parse(xml_file)
-    root = x.getroot()
-
-
-    print("This are the contents of file: ", root["setting"][1])
+    return x
